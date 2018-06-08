e93fb572d3fe41bbec66adfe2dba3a94fceef725
L1ttl3S1st3r <33183682+L1ttl3S1st3r@users.noreply.github.com>
2018-06-08 23:02:49 +0300

    Update Source.cpp

--- c-cpp/lab7_project/lab7_project/Source.cpp
+++ c-cpp/lab7_project/lab7_project/Source.cpp
@@ -2,18 +2,6 @@
 // L1ttl3S1st3r 13.05.2018
 # include "app.h"
 
-// list of testing&debbuging:
-// 1. recursive shows
-// a) pre +
-// b) in +
-// c) post +
-// 2. deleting node +
-// 3. iterative shows
-// a) pre +
-// b) in +
-// c) post +
-// 4. calc of distance
-
 
 int main() {
 	Treap treap = { 0 };
